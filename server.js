a1c8d9d0ec91ab5934057f8952465f40187ed608
Prathyu Prasad PK <67427506+prathyu116@users.noreply.github.com>
2025-05-03 01:29:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect(MONGODB_URI)
 // --- Middleware ---
 // Configure CORS to allow credentials (cookies) from your frontend origin
 app.use(cors({
-    origin: 'https://clinic-queue-manager.vercel.app', // Your frontend URL
+    origin: 'https://clinic-queue-manager.vercel.app', // frontend URL
     credentials: true, // Allow cookies
 }));
 app.use(express.json());
